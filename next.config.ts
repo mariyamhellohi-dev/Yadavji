7cdfadd806b4e2413c2c506309af8974116676cf
unknown boy murtaza <unknownboymurtaza@gmail.com>
2026-02-09 07:11:29 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://docs.li

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'docs.lightburnsoftware.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
